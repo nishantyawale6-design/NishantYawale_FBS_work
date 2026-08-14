574805c055b804191f6a4becef22ea54caf81c9d
Nishant Yawale <nishantyawale6@gmail.com>
2026-08-14 16:45:12 +0530

    Add files via upload

---
+++ Core python/Tests/Test_3/Q1.py
@@ -0,0 +1,15 @@
+n = int(input("enter the number => "))
+count = 0
+num = 2
+
+while(count < n):
+    factors = 0
+    for i in range(1, num + 1):
+        if(num % i == 0):
+            factors = factors + 1
+    if factors == 2:
+        print(num)
+        count = count + 1
+    
+    num = num + 1
+
---
+++ Core python/Tests/Test_3/Q2.py
@@ -0,0 +1,9 @@
+n = int(input("Enter n: "))
+
+fact = 1
+sum = 0
+
+for i in range(1, n+1 ):
+    fact = fact * i
+    sum = sum + (i / fact)
+print("Sum = ", sum)
